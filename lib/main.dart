c61e7c611309394f055ee152348f39ebbe2df819
Yassine Idlhaj <yassinemks7@gmail.com>
2024-04-08 15:31:09 +0000

    UPdates

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ void main() async {
   );
   runApp (MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: "Cinq Etoil Admin",
+    title: "Cinq Etoil Adminn",
     home: ProfileScreen(),
   ));
 

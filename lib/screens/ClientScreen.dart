26ed4455a0787652c6dbfb159b164417fe0e587c
Yassine Idlhaj <yassinemks7@gmail.com>
2024-04-08 13:46:39 +0000

    Updates from yssine

--- lib/screens/ClientScreen.dart
+++ lib/screens/ClientScreen.dart
@@ -2,6 +2,8 @@
 import 'package:animation_search_bar/animation_search_bar.dart';
 import 'package:cinq_etoils/firebase_services/FirebaseServiceProject.dart';
 import 'package:cinq_etoils/firebase_services/FirebaseServiceUser.dart';
+import 'package:cinq_etoils/model/UserModel.dart';
+import 'package:cinq_etoils/model/Users.dart';
 import 'package:cinq_etoils/shared/CustomColors.dart';
 import 'package:cinq_etoils/shared/Widgets/CustomWidgets.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
@@ -61,82 +63,6 @@ class _ClientScreenState extends State<ClientScreen> {
                         ),
                       ),
                       CustomWidgets.customIconButton(
-                        color: CustomColors.green,
-                        func: (){
-                        },
-                        icon:const Icon(Icons.search),
-
-                      ),
-                      DropdownButtonHideUnderline(
-                        child: DropdownButton2<String>(
-                          isExpanded: true,
-                          hint: Text(
-                            'Choisir un projet',
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Theme.of(context).hintColor,
-                            ),
-                          ),
-                          items: [],
-                          value: dropMenuValue,
-                          onChanged: (value) {
-                            setState(() {
-                              dropMenuValue = value;
-                            });
-                          },
-                          buttonStyleData: const ButtonStyleData(
-                            padding: EdgeInsets.symmetric(horizontal: 16),
-                            height: 40,
-                            width: 200,
-                          ),
-                          dropdownStyleData: const DropdownStyleData(
-                            maxHeight: 200,
-                          ),
-                          menuItemStyleData: const MenuItemStyleData(
-                            height: 40,
-                          ),
-                          dropdownSearchData: DropdownSearchData(
-                            searchController: dropDownSearchBarController,
-                            searchInnerWidgetHeight: 50,
-                            searchInnerWidget: Container(
-                              height: 50,
-                              padding: const EdgeInsets.only(
-                                top: 8,
-                                bottom: 4,
-                                right: 8,
-                                left: 8,
-                              ),
-                              child: TextFormField(
-                                expands: true,
-                                maxLines: null,
-                                controller: dropDownSearchBarController,
-                                decoration: InputDecoration(
-                                  isDense: true,
-                                  contentPadding: const EdgeInsets.symmetric(
-                                    horizontal: 10,
-                                    vertical: 8,
-                                  ),
-                                  hintText: 'Chercher un projet...',
-                                  hintStyle: const TextStyle(fontSize: 12),
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            searchMatchFn: (item, searchValue) {
-                              return item.value.toString().contains(searchValue);
-                            },
-                          ),
-                          //This to clear the search value when you close the menu
-                          onMenuStateChange: (isOpen) {
-                            if (!isOpen) {
-                              dropDownSearchBarController.clear();
-                            }
-                          },
-                        ),
-                      ),
-                      CustomWidgets.customIconButton(
                           color: CustomColors.green,
                           func: (){
                           },
@@ -147,7 +73,22 @@ class _ClientScreenState extends State<ClientScreen> {
                     ],
                   ),
                   const Divider(),
-                  
+                  StreamBuilder(
+                      stream: widget._firebaseServiceUser.getUsers(),
+                      builder: (context,snapshot){
+                        return Expanded(
+                            child: ListView.separated(
+                                separatorBuilder: (context,index) => CustomWidgets.verticalSpace(10.0),
+                                itemCount: snapshot.data!.length,
+                                itemBuilder: (context,index){
+                                  AdminUser user =  snapshot.data![index] as AdminUser;
+                                  return CustomWidgets.customCard(user: user,isUser : true,checkbox: true);
+                                },
+                            )
+                        );
+
+                      }
+                  )
                 ],
                   ),
               Positioned(
